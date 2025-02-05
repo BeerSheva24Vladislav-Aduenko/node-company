c214bf70621ea4e2cd628067413a87323d363f37
VladislavAduenko <adyenko.job@gmail.com>
2025-02-05 11:03:39 +0200

    async iterating

--- src/service/Company.mjs
+++ src/service/Company.mjs
@@ -5,15 +5,45 @@ import {
   EMPLOYEE_NOT_FOUND,
   INVALID_EMPLOYEE_TYPE,
 } from "../exceptions/exceptions.mjs";
-import { readFile, writeFile } from "node:fs/promises";
+import {readFile,writeFile} from 'node:fs/promises'
 export default class Company {
   #employees; //key - id, value - Employee {id:123, empl: {123,...}}
   #departments; //key department, value array of employees working in the department
-  constructor() {
+  #predicate;
+  constructor(predicate) {
     this.#employees = {};
     this.#departments = {};
+    this.setPredicate(predicate);
+    this.#setIterable();
+
   }
-  
+  #setIterable() {
+        
+        this[Symbol.asyncIterator] = async function* () {
+            const values = Object.values(this.#employees);
+            let indexCur = -1;
+
+            while(true) {
+                const {index, value} = this.#getNext(indexCur, values);
+                if(!value) {
+                    break;
+                }
+                yield value;
+                indexCur = index;
+            }
+        }
+  }
+  #getNext(index, values) {
+    let value;
+    index++;
+   while((value = values[index]) && !this.#predicate(value)) {
+    index++;
+   }
+   return {index, value};
+  }
+  setPredicate(predicate) {
+    this.#predicate = predicate ?? (e => true);
+  }
   async addEmployee(employee) {
     if (!(employee instanceof Employee)) {
       throw Error(INVALID_EMPLOYEE_TYPE(employee));
@@ -22,21 +52,19 @@ export default class Company {
       throw Error(EMPLOYEE_ALREADY_EXISTS(employee.id));
     }
     this.#employees[employee.id] = employee;
-    this.#addDepartments(employee);
+    this.#addDepartments(employee)
   }
-
-  #addDepartments(employee) {
+  #addDepartments(employee){
     const dep = employee.department;
-    if (!this.#departments[dep]) {
-      this.#departments[dep] = [];
+    if(!this.#departments[dep]){
+        this.#departments[dep] = [];
+
     }
     this.#departments[dep].push(employee);
   }
-
   async getEmployee(id) {
     return this.#employees[id] ?? null;
   }
-
   async removeEmployee(id) {
     if (!this.#employees[id]) {
       throw Error(EMPLOYEE_NOT_FOUND(id));
@@ -44,27 +72,23 @@ export default class Company {
     this.#removeDepartments(this.#employees[id]);
     delete this.#employees[id];
   }
-
-  #removeDepartments(employee) {
+  #removeDepartments(employee){
     const employees = this.#departments[employee.department];
-    const index = employees.findIndex((e) => e.id === employee.id);
-    employees.splice(index, 1);
+    const index = employees.findIndex(e => e.id === employee.id);
+    employees.splice(index,1);
     employees.length == 0 && delete this.#departments[employee.department];
   }
-
   async getDepartmentBudget(department) {
     let res = 0;
     const employees = this.#departments[department];
-    if (employees) {
-      res = employees.reduce((bud, cur) => bud + cur.computeSalary(), 0);
+    if(employees) {
+        res = employees.reduce((bud, cur) => bud + cur.computeSalary(),0);
     }
     return res;
   }
-
   async getDepartments() {
     return Object.keys(this.#departments).toSorted();
   }
-
   async getManagersWithMostFactor() {
     const managers = Object.values(this.#employees).filter(
       (e) => e instanceof Manager
@@ -87,12 +111,12 @@ export default class Company {
   }
   async saveToFile(fileName) {
     const employeesJSON = JSON.stringify(Object.values(this.#employees));
-    await writeFile(fileName, employeesJSON, "utf8");
+    await writeFile(fileName,employeesJSON,'utf8');
   }
 
   async restoreFromFile(fileName) {
-    const employeesPlain = JSON.parse(await readFile(fileName, "utf8"));
-    const employees = employeesPlain.map((e) => Employee.fromPlainObject(e));
-    employees.forEach((e) => this.addEmployee(e));
+    const employeesPlain = JSON.parse(await readFile(fileName, 'utf8'));
+    const employees = employeesPlain.map(e => Employee.fromPlainObject(e));
+    employees.forEach(e => this.addEmployee(e));
   }
 }
--- test/Company.test.mjs
+++ test/Company.test.mjs
@@ -49,7 +49,6 @@ async function fillCompany() {
     company.addEmployee(empl);
   }
 }
-
 describe("tests of company updating", () => {
   beforeEach(async () => await fillCompany());
   it(" adding new Employee", async () => {
@@ -62,7 +61,6 @@ describe("tests of company updating", () => {
       INVALID_EMPLOYEE_TYPE(2)
     );
   });
-
   it("removing existing Employee", async () => {
     await expect(company.removeEmployee(ID4)).rejects.toThrowError(
       EMPLOYEE_NOT_FOUND(ID4)
@@ -71,7 +69,6 @@ describe("tests of company updating", () => {
     await company.addEmployee(empl1);
   });
 });
-
 describe("tests of company non-updating", async () => {
   beforeEach(async () => await fillCompany());
   it("getting Employee object test", async () => {
@@ -80,7 +77,6 @@ describe("tests of company non-updating", async () => {
     expect(empl instanceof WageEmployee).toBeTruthy();
     expect(await company.getEmployee(ID4)).toBeNull();
   });
-
   it("getting Managers with maximal factor", async () => {
     await company.addEmployee(new Manager(ID4, DEPARTMENT1, SALARY1, FACTOR2));
     const managersExpected = [
@@ -100,11 +96,9 @@ describe("tests of company non-updating", async () => {
     await company.removeEmployee(ID2);
     expect(await company.getManagersWithMostFactor()).toEqual([]);
   });
-
   it("get all departments", async () => {
     expect(await company.getDepartments()).toEqual([DEPARTMENT2, DEPARTMENT1]);
   });
-
   it("get department budget", async () => {
     expect(await company.getDepartmentBudget(DEPARTMENT1)).toBe(
       SALARY1 + WAGE1 * HOURS1 + SALARY2 * FACTOR1
@@ -116,7 +110,6 @@ describe("tests of company non-updating", async () => {
     expect(await company.getDepartmentBudget(DEPARTMENT4)).toBe(0);
   });
 });
-
 describe("persistence tests", async () => {
   beforeEach(async () => await fillCompany());
   it("save / restore persistence test", async () => {
@@ -140,3 +133,27 @@ describe("persistence tests", async () => {
     }
   });
 });
+describe("async iterator test", () => {
+  beforeEach(async () => await fillCompany());
+
+  it("iterating all objects", async () => {
+    await runTest([empl1, empl2, empl3], undefined, company);
+  });
+  it("iterating objects with basic salary greater than 1000", async () => {
+    await runTest([empl2, empl3], (e) => e.getBasicSalary() > 1000, company);
+  });
+  it("iterating objects with basic salary less than 1000", async () => {
+    await runTest([], (e) => e.getBasicSalary() < 1000, company);
+  });
+});
+async function runTest(expected, predicate, company) {
+  const comp = (e1, e2) => e1.getId() - e2.getId();
+  company.setPredicate(predicate);
+  expected = expected.toSorted(comp);
+  const actual = [];
+  for await (const empl of company) {
+    actual.push(empl);
+  }
+  actual.sort(comp);
+  expect(actual).toEqual(expected);
+}
